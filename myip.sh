0f0a779ed4c7d11b8e620a45e8f59f0888610d29
Ariel <i@arielvb.com>
2022-12-01 10:17:34 +0100

    myip.sh: support for Linux.

--- myip.sh
+++ myip.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-# Prints all network IP and the public IP (MACOS only)
-for i in $(seq 0 $(ipconfig ifcount)); do
-	echo "en$i: $(ipconfig getifaddr en$i)"
-done
+# myip.sh
+# =======
+# Print all network IP and the public IP
+#
+# On Mac it will also dsisplay the network device name.
+
+
+if [[ $(uname) == 'Linux' ]]; then
+
+	ifconfig | awk '/inet / {gsub("addr:", "", $2); print $2}'
+else
+	#We assume we are on Mac OS
+
+	for i in $(seq 0 $(ipconfig ifcount)); do
+		echo "en$i: $(ipconfig getifaddr en$i)"
+	done
+fi
+
 echo "Public IP: $(dig +short myip.opendns.com @resolver1.opendns.com)"
