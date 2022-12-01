0273476e88eb6ca9ad131238f3f21d6ab89fe44e
Ariel <i@arielvb.com>
2022-12-01 09:43:33 +0100

    hidden.sh v0.3.

--- hidden.sh
+++ hidden.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 #######################################################################
 # hidden.sh
-# arielvb - http://www.arielvb.com
-# Shows or hiddes hidden files on MacOS X Finder 
-# Version: 0.2
+# Shows or hides hidden files on MacOS X Finder
+# Version: 0.3
 # Usage: hidden [on|off]
+#     on 		to hide files
+#	  off		to display hidden files.
 # Changelog:
-#	0.1 first version
-#	0.2 On/off style
+#	0.1 first version.
+#	0.2 On/off style.
+#	0.3 Change finder -> Finder, 0/1 -> FALSE/TRUE, improve feedback.
 #######################################################################
 hiddenwriter(){
-	defaults write com.apple.finder AppleShowAllFiles $1
+	defaults write com.apple.Finder AppleShowAllFiles $1
 	killall Finder
 }
-CURRENT=`defaults read com.apple.finder AppleShowAllFiles`;
+CURRENT=`defaults read com.apple.Finder AppleShowAllFiles`;
 if [[ $BASH_ARGC < 0 ]]; then
-	if [[ $CURRENT == '1' ]]; then
-		echo 'off'
-	else
+	if [[ $CURRENT == 'FALSE' ]]; then
 		echo 'on'
+	else
+		echo 'off'
 	fi
 	exit
 fi
@@ -28,4 +30,6 @@ if [[ ($1 == 'off') && ($CURRENT == 'FALSE')]]; then
 elif [[ ($1 == 'on') && ($CURRENT == 'TRUE')]]; then
 	hiddenwriter FALSE
 	echo 'Hidden files not visible'
+else
+	echo "Nothing changed"
 fi
